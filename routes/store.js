62d6c37b7ed9c1decd703ea0623535d9a40c01a3
djibba22 <johnwaynedinsmore@gmail.com>
2021-10-05 15:35:57 -0400

    fixing up the response to the key request

--- routes/store.js
+++ routes/store.js
@@ -24,7 +24,7 @@ keyRouter.route('/:key')
     .then(key => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
-        res.send(`Here is your key ${key.apiKey}`);
+        res.json(key);
     })
     .catch(err => next(err));
 })
