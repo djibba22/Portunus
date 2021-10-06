c168375059078bd609f681402392bd6c6535db3f
djibba22 <johnwaynedinsmore@gmail.com>
2021-10-05 20:10:06 -0400

    added some css to the page for copying the fetch

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,7 +1,9 @@
 const addButton = document.querySelector(".add");
-
+const deleteBtn = document.querySelector(".delete");
+const removeBtn = document.querySelector(".remove");
+const badMsg = document.querySelector(".bad-note");
+const goodMsg = document.querySelector(".good-note");
 const storeKey = (e) => {
-    console.log("clicking");
     e.preventDefault();
     // grab the values
     const data = {
@@ -9,15 +11,41 @@ const storeKey = (e) => {
         apiKey: document.getElementById("keyValue").value
     }
 
-    // post them to the server
-    postKey("/store",data).then((yourKey) => {
-            //chack the key which was returned
-            console.log(`key from db=${JSON.stringify(yourKey)}`);
-    });
-    
-    // Empty the form
-    document.getElementById("keyName").value = ""
-    document.getElementById("keyValue").value = ""
+    if (!data.name.length > 0 || !data.apiKey.length > 0){
+      // show the flash message
+      badMsg.classList.remove("hide");
+      setTimeout(() => {
+        location.reload();
+      }, 1000);
+    }else{
+      // post them to the server
+      postKey("/store",data).then((yourKey) => {
+        document.querySelector(".snippet").innerHTML = 
+        `<h2>Place this code snippet into your JS file</h2>
+        <pre class="box">
+        <code>
+          // This calls the API, just update the url to have your key's name.
+          async function fetchKey() {
+              const url = 'https://cors-anywhere.herokuapp.com/https://yorkieportunus.herokuapp.com/store/${yourKey.name}'
+              const response = await fetch(url);
+              const key = await response.json();
+              return key;
+          }
+          // Call this wherever you need your key.
+          fetchKey().then((key) => {
+              secretKey = key.apiKey;
+          });
+        </code>
+      </pre>`;
+        //check the key which was returned
+        console.log(`key from db=${JSON.stringify(yourKey)}`);
+      });
+      // Empty the form
+      document.getElementById("keyName").value = ""
+      document.getElementById("keyValue").value = ""
+    }
+  
+
 }
 
 async function postKey(url,data) {
@@ -30,10 +58,19 @@ async function postKey(url,data) {
       },
       body: JSON.stringify(data)
     });
+
+    response.ok ? document.querySelector(".notification").classList.remove("hide"):console.log("Error in the request")
     // get the key out of the response
     const key =  await response.json();
     return  key// parses JSON response into native JavaScript objects
-  }
+}
+
+const removeNote = (e) => {
+    console.log("clicking");
+    e.target.parentElement.remove();
+}
 
 // add click listener
 addButton.addEventListener("click", storeKey);
+deleteBtn.addEventListener("click",removeNote); 
+removeBtn.addEventListener("click",removeNote); 
